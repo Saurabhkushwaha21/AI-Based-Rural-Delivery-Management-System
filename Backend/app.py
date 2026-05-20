7922b3b5583faa69021cc1888b935cb6e793254b
Saurabh Kushwaha <kushwahasaurabh639@gmail.com>
2026-05-20 21:59:26 +0530

    fixed cors and auth routes

--- Backend/app.py
+++ Backend/app.py
@@ -29,8 +29,7 @@ Base.metadata.create_all(bind=engine)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500"
+       "https://ai-based-rural-delivery-management.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
